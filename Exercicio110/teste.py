0f1eba7906cbbed840d98221ca3e94ff53c80359
gersonpas <sobrinho.gerson@gmail.com>
2021-04-20 18:23:49 -0300

    Initial commit Programa de Calculo de Valores

---
+++ Exercicio110/teste.py
@@ -0,0 +1,5 @@
+from Exercicio110 import moeda
+
+p = float(input('Digite o Preço: R$'))
+moeda.resumo(p)
+
---
+++ Exercicio111/teste.py
@@ -0,0 +1,5 @@
+from Exercicio111.utilidadesCev import moeda
+from  Exercicio111.utilidadesCev import dado
+p = float(input('Digite o Preço: R$'))
+moeda.resumo(p,25,15)
+
