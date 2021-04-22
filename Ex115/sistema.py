1a2e59a9b834c8f5de0a7bc8eaeddbfb1eca1006
gersonpas <sobrinho.gerson@gmail.com>
2021-04-22 18:55:43 -0300

    Versão v1.3

--- Ex115/sistema.py
+++ Ex115/sistema.py
@@ -13,7 +13,10 @@ while True:
         # opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq,nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do Sistema...Até Logo!')
         break
